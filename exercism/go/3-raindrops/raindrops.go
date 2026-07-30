6aef9170abb8348e351e0adb5a4a154549cf9eaf
Joaquin Gonzalez <joagonzalez@gmail.com>
2026-07-30 12:04:52 -0300

    day 2 and 3 katas

---
+++ exercism/go/3-raindrops/raindrops.go
@@ -0,0 +1,26 @@
+package raindrops
+
+import "strconv"
+
+func Convert(number int) string {
+	var result string
+	result = ""
+
+	if number%3 == 0 {
+		result += "Pling"
+	}
+
+	if number%5 == 0 {
+		result += "Plang"
+	}
+
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if result == "" {
+		result = strconv.Itoa(number)
+	}
+
+	return result
+}
